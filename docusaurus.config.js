2d41faf80177688cc6c43905d5006214a0422544
dodokyo <ypd03008@naver.com>
2023-07-23 11:16:44 +0900

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,7 +98,7 @@ const config = {
             position: "left",
             label: "DevTech",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://www.youtube.com/@dodocoding",
             label: "취업상담소 Youtube",
@@ -138,10 +138,10 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
                 label: "GitHub",
                 href: "https://github.com/facebook/docusaurus",
